09f5be1994434c28ad0c05bfc82da3e238f5af37
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-10-19 14:51:10 +0530

    project service

---
+++ ProjectManagmentSystemBackend/src/main/java/com/project_management/belfazt/services/ProjectService.java
@@ -0,0 +1,20 @@
+package com.project_management.belfazt.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project_management.belfazt.domain.Project;
+import com.project_management.belfazt.repository.ProjectRepository;
+
+@Service
+public class ProjectService {
+
+	@Autowired
+	ProjectRepository projectRepository;
+	
+	public Project saveOrUpdateProject(Project project) {
+		
+		return projectRepository.save(project);
+	}
+	
+}
