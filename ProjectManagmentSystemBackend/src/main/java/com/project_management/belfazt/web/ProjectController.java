eb9c7ff77383adbcd8fad33f6c3c2feaa9e70c66
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-10-19 17:41:54 +0530

    validation service

--- ProjectManagmentSystemBackend/src/main/java/com/project_management/belfazt/web/ProjectController.java
+++ ProjectManagmentSystemBackend/src/main/java/com/project_management/belfazt/web/ProjectController.java
@@ -1,15 +1,11 @@
 package com.project_management.belfazt.web;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project_management.belfazt.domain.Project;
 import com.project_management.belfazt.services.ProjectService;
+import com.project_management.belfazt.services.ValidationErrorService;
 
 @RestController
 @RequestMapping("/api/project")
@@ -25,16 +22,14 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private ValidationErrorService validationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 		
-		if(result.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<String, String>();
-			for(FieldError error : result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);	
-		}
+		ResponseEntity<?> errorMap = validationErrorService.validateError(result);
+		if(errorMap != null) return errorMap;
 		
 		project = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
