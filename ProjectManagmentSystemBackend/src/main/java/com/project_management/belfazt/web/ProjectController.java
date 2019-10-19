a8e4e3d0691ee694681431bc04c82f215f95c598
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-10-19 17:23:34 +0530

    validation

--- ProjectManagmentSystemBackend/src/main/java/com/project_management/belfazt/web/ProjectController.java
+++ ProjectManagmentSystemBackend/src/main/java/com/project_management/belfazt/web/ProjectController.java
@@ -1,11 +1,15 @@
 package com.project_management.belfazt.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +28,13 @@ public class ProjectController {
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 		
-		if(result.hasErrors())
-			return new ResponseEntity<String>("Invalid project object", HttpStatus.BAD_REQUEST);
+		if(result.hasErrors()) {
+			Map<String, String> errorMap = new HashMap<String, String>();
+			for(FieldError error : result.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);	
+		}
 		
 		project = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
