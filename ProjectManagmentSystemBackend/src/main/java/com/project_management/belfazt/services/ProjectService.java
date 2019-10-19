31e1a3747b90b1fc52e95a30d61b4a161ebbbea0
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-10-19 19:13:15 +0530

    delete project

--- ProjectManagmentSystemBackend/src/main/java/com/project_management/belfazt/services/ProjectService.java
+++ ProjectManagmentSystemBackend/src/main/java/com/project_management/belfazt/services/ProjectService.java
@@ -36,4 +36,14 @@ public class ProjectService {
 	public Iterable<Project> findAllProjects(){
 		return projectRepository.findAll();
 	}
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		Project project = findProjectByIdentifier(projectId);
+		
+		if(project == null) {
+			throw new ProjectIdException("Project ID not "+ projectId +" found");
+		}
+		
+		projectRepository.delete(project);
+	}
 }
